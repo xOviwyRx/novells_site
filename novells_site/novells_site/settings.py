7d766b5541b843e6e885c2beb3950fe874eb47be
Marat home <vmarat95@gmail.com>
2021-04-11 15:37:18 +0300

    0.8.2.5 prepare

--- novells_site/novells_site/settings.py
+++ novells_site/novells_site/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3uby7wv_r$o0gyvt1wmx+j9rv!xuc@l%0r45!+68%nu5=8$+wd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 if DEBUG:
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['31.31.203.141', '127.0.0.1']
 else:
     ALLOWED_HOSTS = ['31.31.203.141', '127.0.0.1']
 
