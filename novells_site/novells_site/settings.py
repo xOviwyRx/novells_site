9fd790bd1a164f6adf7ad867707f041799a43888
maratDC <marat@dc.ru>
2021-04-20 18:24:03 +0300

    0.8.6 editing pages

--- novells_site/novells_site/settings.py
+++ novells_site/novells_site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3uby7wv_r$o0gyvt1wmx+j9rv!xuc@l%0r45!+68%nu5=8$+wd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 if DEBUG:
