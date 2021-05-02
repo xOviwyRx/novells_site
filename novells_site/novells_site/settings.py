73d1f715c0a5f658b2e42da1baeefabbcdb97f7b
Marat home <vmarat95@gmail.com>
2021-05-02 07:57:16 +0300

    add news to whatewer

--- novells_site/novells_site/settings.py
+++ novells_site/novells_site/settings.py
@@ -188,3 +188,18 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'haxballcis@gmail.com'
 EMAIL_HOST_PASSWORD = 'ebalvrotmarokko32288'
+
+TINYMCE_DEFAULT_CONFIG = {
+    "height": "500px",
+    "width": "80%",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
+    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
+
+}
